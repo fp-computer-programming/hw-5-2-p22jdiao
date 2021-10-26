6a20c55c5206ac17373a81dd2ffcb05fcb41f33b
p22jdiao <p22jdiao@fairfieldprep.org>
2021-10-26 10:20:42 -0400

    q5

---
+++ hw5-2.py
@@ -0,0 +1,19 @@
+# Author: JD 10/26/2021
+
+import math
+
+# Question 1
+print(math.sqrt(529))
+
+# Question 2
+print(math.sin(0))
+
+# Question 3
+print(math.cos(math.pi))
+
+# Question 4
+print(math.pow(2.5,5))
+
+# Question 5
+print(math.pow(math.e,4))
+
